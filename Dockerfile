faf18f7c1e64cf332560928ab9857ced901669eb
Tulir Asokan <tulir@maunium.net>
2024-08-25 13:35:19 +0300

    docker: add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM golang:1-alpine3.20 AS builder
+
+RUN apk add --no-cache git ca-certificates build-base su-exec olm-dev
+
+COPY . /build
+WORKDIR /build
+RUN go build -o /usr/bin/mautrix-gvoice
+
+FROM alpine:3.20
+
+ENV UID=1337 \
+    GID=1337
+
+RUN apk add --no-cache ffmpeg su-exec ca-certificates olm bash jq yq-go curl
+
+COPY --from=builder /usr/bin/mautrix-gvoice /usr/bin/mautrix-gvoice
+COPY --from=builder /build/docker-run.sh /docker-run.sh
+VOLUME /data
+
+CMD ["/docker-run.sh"]
---
+++ docker-run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [[ -z "$GID" ]]; then
+	GID="$UID"
+fi
+
+# Define functions.
+function fixperms {
+	chown -R $UID:$GID /data
+}
+
+if [[ ! -f /data/config.yaml ]]; then
+	/usr/bin/mautrix-gvoice -c /data/config.yaml -e
+	echo "Didn't find a config file."
+	echo "Copied default config file to /data/config.yaml"
+	echo "Modify that config file to your liking."
+	echo "Start the container again after that to generate the registration file."
+	exit
+fi
+
+if [[ ! -f /data/registration.yaml ]]; then
+	/usr/bin/mautrix-gvoice -g -c /data/config.yaml -r /data/registration.yaml || exit $?
+	echo "Didn't find a registration file."
+	echo "Generated one for you."
+	echo "See https://docs.mau.fi/bridges/general/registering-appservices.html on how to use it."
+	exit
+fi
+
+cd /data
+fixperms
+exec su-exec $UID:$GID /usr/bin/mautrix-gvoice
