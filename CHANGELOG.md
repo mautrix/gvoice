a4541c19d3af65d8b0a76cc98f585fce4e9192bc
Tulir Asokan <tulir@maunium.net>
2025-07-16 11:44:44 +0300

    Bump version to v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.1.1 (2025-07-16)
+
+* Updated Docker image to Alpine 3.22.
+
 # v0.1.0 (2024-09-16)
 
 Initial release.
--- cmd/mautrix-gvoice/main.go
+++ cmd/mautrix-gvoice/main.go
@@ -33,7 +33,7 @@ var m = mxmain.BridgeMain{
 	Name:        "mautrix-gvoice",
 	Description: "A Matrix-Google Voice puppeting bridge",
 	URL:         "https://github.com/mautrix/gvoice",
-	Version:     "0.1.0",
+	Version:     "0.1.1",
 	Connector:   c,
 }
 
